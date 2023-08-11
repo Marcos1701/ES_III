b300a7df541b07683e0b4a09c91956a3b093f228
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2023-08-11 12:56:26 -0300

    Update Menu.java

--- Atividade/src/Menu.java
+++ Atividade/src/Menu.java
@@ -5,12 +5,10 @@ public class Menu {
         System.out.println("1 - Cadastrar biblioteca");
         System.out.println("2 - Cadastrar livro");
         System.out.println("3 - Acessar biblioteca");
-        System.out.println("4 - Remover biblioteca");
-        System.out.println("5 - Remover livro");
-        System.out.println("6 - Listar bibliotecas");
-        System.out.println("7 - Listar livros");
-        System.out.println("8 - Alterar biblioteca");
-        System.out.println("9 - Alterar livro");
+        System.out.println("4 - Remover livro");
+        System.out.println("5 - Listar bibliotecas");
+        System.out.println("6 - Listar livros");
+        System.out.println("7 - Alterar livro");
         System.out.println("\n\n0 - Sair");
     }
 
@@ -31,34 +29,29 @@ public class Menu {
         System.out.println("------- Alterar Biblioteca --------");
         System.out.println("\nSelecione uma opção:");
         System.out.println("1 - Alterar nome");
-        System.out.println("2 - alterar livros");
         System.out.println("\n\n0 - Sair");
     }
 
-    public static void menu_de_acesso_biblioteca() {
+    // public static void menu_de_remocao_livro() {
 
-        System.out.println("------- Acessar Biblioteca --------");
+    //     System.out.println("------- Excluir Livro --------");
+    //     System.out.println("\nSelecione uma opção:");
+    //     System.out.println("1 - Remover livro por titulo");
+    //     System.out.println("2 - Remover livro por id");
+    //     System.out.println("\n\n0 - Sair");
+    // }
+
+    public static void menu_biblioteca(){
+        System.out.println("------- Menu  --------");
         System.out.println("\nSelecione uma opção:");
-        System.out.println("1 - Acessar biblioteca por nome");
-        System.out.println("2 - Acessar biblioteca por id");
+        System.out.println("1 - Visualizar dados Biblioteca");
+        System.out.println("2 - Visualizar Livros");
+        System.out.println("3 - remover Livro");
+        System.out.println("4 - Adicionar Livro");
+        System.out.println("5 - Alterar Biblioteca");
+        System.out.println("6 - Excluir Biblioteca");
         System.out.println("\n\n0 - Sair");
     }
 
-    public static void menu_de_remocao_livro() {
 
-        System.out.println("------- Remover Livro --------");
-        System.out.println("\nSelecione uma opção:");
-        System.out.println("1 - Remover livro por titulo");
-        System.out.println("2 - Remover livro por id");
-        System.out.println("\n\n0 - Sair");
-    }
-
-    public static void menu_de_remocao_biblioteca() {
-
-        System.out.println("------- Remover Biblioteca --------");
-        System.out.println("\nSelecione uma opção:");
-        System.out.println("1 - Remover biblioteca por nome");
-        System.out.println("2 - Remover biblioteca por id");
-        System.out.println("\n\n0 - Sair");
-    }
 }
